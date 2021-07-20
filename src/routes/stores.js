9e1ee9f63bf096f33e0c53eec7c50f3d16c2685a
Naveen <catchspider2002@gmail.com>
2021-07-21 00:15:02 +0530

    Added Romanian translations

--- src/routes/stores.js
+++ src/routes/stores.js
@@ -1,5 +1,5 @@
 import { writable } from 'svelte/store';
 
 export const themeStore = writable("light");
-export const langStore = writable("es");
-export const webStore = writable("https://es.pattern.monster");
+export const langStore = writable("ro");
+export const webStore = writable("https://ro.pattern.monster");
